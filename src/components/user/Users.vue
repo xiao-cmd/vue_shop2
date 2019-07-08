364d9006e935978cae9f53699ae5ff3deedc5144
xiao-cmd <1352376498@qq.com>
2019-07-08 21:39:44 +0800

    用户列表

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -19,25 +19,26 @@
           <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
+    </el-card>
 
         <el-table :data="userlist" border stripe>
-            <el-table-column type="index"></el-table-column>
-            <el-table-column label="姓名" prop="username"></el-table-column>
-            <el-table-column label="邮箱" prop="email"></el-table-column>
-            <el-table-column label="电话" prop="mobile"></el-table-column>
-            <el-table-column label="角色" prop="role_name"></el-table-column>
-            <el-table-column label="状态" prop="mg_state">
-                <template slot-scope="scope">   
-                    <!-- {{scope.row}} -->
-                    <el-switch v-model="scope.row.mg_state"></el-switch>
-                </template> 
-            </el-table-column>
-            <el-table-column label="操作"></el-table-column>
+          <el-table-column type="index"></el-table-column>
+          <el-table-column label="姓名" prop="username"></el-table-column>
+          <el-table-column label="邮箱" prop="email"></el-table-column>
+          <el-table-column label="电话" prop="mobile"></el-table-column>
+          <el-table-column label="角色" prop="role_name"></el-table-column>
+          <el-table-column label="状态" prop="mg_state">
+            <template slot-scope="scope">
+              <!-- {{scope.row}} -->
+              <el-switch v-model="scope.row.mg_state"></el-switch>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作"></el-table-column>
         </el-table>
-
-    </el-card>
+     
   </div>
 </template>
+
 <script>
 export default {
   data() {
