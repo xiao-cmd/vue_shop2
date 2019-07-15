43311f7bd66165a12321cb50a0f2bf265dfbee64
xiao-cmd <1352376498@qq.com>
2019-07-15 14:50:11 +0800

    商品列表

---
+++ src/components/order/Order.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+     <!-- 卡片视图区域 -->
+    <el-card>
+      <!--添加商品按钮 -->
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" clearable >
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-card>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    created(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
+
